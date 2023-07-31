03a686b888a620c9551757dbeaab3d8571d84cf9
Allstreamer <48365544+Allstreamer@users.noreply.github.com>
2023-08-01 00:22:02 +0200

    Basic Scaffold

--- lua/all/fuzzy.lua
+++ lua/all/fuzzy.lua
@@ -1,1 +1,27 @@
-print('Hello world')
+-- All.fuzzy
+
+local public = {}
+local internal = {}
+
+public.config = {}
+
+public.setup = function(config)
+
+end
+
+public.telescope = function(opts)
+    opts = opts or {}
+    return require('telescope.sorters').Sorter:new({
+        scoring_function = function(_, prompt, line)
+           -- Return Score from 0 to Inf (Zero being the closest to the prompt)
+           return 0
+        end,
+
+        highlighter = function(_, prompt, display)
+           -- Return Score from 0 to Inf (Zero being the closest to the prompt)
+           return 0
+        end,
+    })
+end
+
+return public
